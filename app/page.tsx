3dbdd513a1e577862823cd1ab714f40556ca9ef8
Dima <dmitriy.andrushchenko77@gmail.com>
2026-02-05 00:17:30 +0200

    refactor: remove unused async Page component and related Supabase client code

--- app/page.tsx
+++ app/page.tsx
@@ -13,24 +13,6 @@ import { Lesson } from "@/lib/types/lesson";
 import { formatDateString } from "@/lib/utils/dateUtils";
 import { ProtectedPage } from "@/components/ProtectedPage";
 
-import { createClient } from "@/lib/supabase/server";
-import { cookies } from "next/headers";
-
-export default async function Page() {
-  const cookieStore = await cookies();
-  const supabase = createClient(cookieStore);
-
-  const { data: todos } = await supabase.from("todos").select();
-
-  return (
-    <ul>
-      {todos?.map((todo) => (
-        <li>{todo}</li>
-      ))}
-    </ul>
-  );
-}
-
 export default function Home() {
   const { lessons, loading, error, addLesson, updateLesson, deleteLesson } =
     useLessons();
